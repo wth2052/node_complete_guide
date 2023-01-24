84ea2a88e1151e64cc442ab80516692f722e0a1e
“Woo <wth2052@gmail.com>
2023-01-24 22:06:44 +0900

    section:6

---
+++ routes/shop.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {prods: products, docTitle: 'Shop'});
+});
+
+module.exports = router;
