f713a294ba94f63ed9e52eedc85da586e4d5dee0
“Woo <wth2052@gmail.com>
2023-01-25 20:09:01 +0900

    왜 모델을 사용해야 하는가? DB보단 못하지만.. 내부 변수 보단 낫다!

---
+++ models/product.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
+
+  save() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      //이 콜백을 호출하게되면 -> controllers에 가서 FetchAll을 호출하는 컨트롤러로 이동, 함수 내부로 전달함
+      cb(JSON.parse(fileContent));
+    });
+  }
+};
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,11 @@
+const path = require('path');
 
 const express = require('express');
 
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {prods: products, docTitle: 'Shop'});
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
