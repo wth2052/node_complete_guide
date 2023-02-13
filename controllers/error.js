ca5d527579b17870f3870fbfa4f2794b60eb041e
“Woo TaeHyeon <wth2052@gmail.com>
2023-02-13 21:34:22 +0900

    수정중... isLoggedIn undefined Issue.. 내일 다시해결

--- controllers/error.js
+++ controllers/error.js
@@ -2,6 +2,7 @@ exports.get404 = (req, res, next) => {
   res.status(404).render('404', {
     pageTitle: '페이지를 찾을 수 없습니다.',
     path: '/404',
+    isAuthenticated: req.session.isLoggedIn
      });
 };
 
@@ -9,5 +10,6 @@ exports.get500 = (req, res, next) => {
   res.status(500).render('500', {
     pageTitle: '에러 발생!',
     path: '/500',
+    isAuthenticated: req.session.isLoggedIn
   });
 };
