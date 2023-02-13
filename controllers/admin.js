a56de66cbd5bac16d0fa43d7c95670c0a5f1f877
“Woo TaeHyeon <wth2052@gmail.com>
2023-02-13 21:34:22 +0900

    수정중... isLoggedIn undefined Issue.. 내일 다시해결

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,15 +17,30 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
+  const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
+  if (!image) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: '포켓몬 추가',
+      path: '/admin/add-product',
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        price: price,
+        description: description
+      },
+      errorMessage: '업로드 하려는 파일이 이미지가 아닙니다.',
+      validationErrors: []
+    });
+  }
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     console.log(errors.array());
     return res.status(422).render('admin/edit-product', {
-      pageTitle: '포켓몬 추가',
+      pageTitle: 'Add Product',
       path: '/admin/add-product',
       editing: false,
       hasError: true,
@@ -39,6 +54,8 @@ exports.postAddProduct = (req, res, next) => {
       validationErrors: errors.array()
     });
   }
+
+  const imageUrl = image.path;
 
   const product = new Product({
     title: title,
@@ -93,20 +110,19 @@ exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
+  const image = req.file;
   const updatedDesc = req.body.description;
 
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     return res.status(422).render('admin/edit-product', {
-      pageTitle: 'Edit Product',
+      pageTitle: '포켓몬 수정',
       path: '/admin/edit-product',
       editing: true,
       hasError: true,
       product: {
         title: updatedTitle,
-        imageUrl: updatedImageUrl,
         price: updatedPrice,
         description: updatedDesc,
         _id: prodId
@@ -116,6 +132,7 @@ exports.postEditProduct = (req, res, next) => {
     });
   }
 
+
   Product.findById(prodId)
     .then(product => {
       if (product.userId.toString() !== req.user._id.toString()) {
@@ -124,7 +141,9 @@ exports.postEditProduct = (req, res, next) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
+      if (image) {
+        product.imageUrl = image.path;
+      }
       return product.save().then(result => {
         console.log('포켓몬정보가 정상적으로 업데이트 되었습니다.');
         res.redirect('/admin/products');
@@ -136,6 +155,7 @@ exports.postEditProduct = (req, res, next) => {
       return next(error);
     });
 };
+
 
 exports.getProducts = (req, res, next) => {
   Product.find({ userId: req.user._id })
