cb5ea08ad67159593ce463374825da1a6c44e24c
“Woo TaeHyeon <wth2052@gmail.com>
2023-02-13 16:21:43 +0900

    HOTFIX: conflict

--- routes/auth.js
+++ routes/auth.js
@@ -10,15 +10,14 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-<<<<<<< HEAD
 router.post(
   '/login',
   [
     body('email')
       .isEmail()
-      .withMessage('올바른 이메일 형식을 입력해주세요. 예: example@gmail.com')
+      .withMessage('올바른 이메일 주소가 아닙니다.')
       .normalizeEmail(),
-    body('password', '입력한 비밀번호가 너무 짧습니다. 5자 이상으로 입력해야 합니다.')
+    body('password', '입력한 비밀번호가 너무 짧습니다.')
       .isLength({ min: 5 })
       .isAlphanumeric()
       .trim()
@@ -31,7 +30,7 @@ router.post(
   [
     check('email')
       .isEmail()
-      .withMessage('Please enter a valid email.')
+      .withMessage('올바른 이메일 주소가 아닙니다.')
       .custom((value, { req }) => {
         // if (value === 'test@test.com') {
         //   throw new Error('This email address if forbidden.');
@@ -40,7 +39,7 @@ router.post(
         return User.findOne({ email: value }).then(userDoc => {
           if (userDoc) {
             return Promise.reject(
-              'E-Mail exists already, please pick a different one.'
+              '이메일 주소가 이미 가입되어 있습니다. 다른 이메일을 입력해주세요.'
             );
           }
         });
@@ -48,32 +47,27 @@ router.post(
       .normalizeEmail(),
     body(
       'password',
-      'Please enter a password with only numbers and text and at least 5 characters.'
+      '비밀번호는 5자 이상, 문자와 숫자로만 입력할 수 있습니다.'
     )
       .isLength({ min: 5 })
       .isAlphanumeric()
       .trim(),
-
-    body('confirmPassword').trim().custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error('Passwords have to match!');
-      }
-      return true;
-    })
+    body('confirmPassword')
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('비밀번호와 비밀번호 확인이 일치하지 않습니다.');
+        }
+        return true;
+      })
   ],
   authController.postSignup
 );
-=======
-router.post('/login', authController.postLogin);
->>>>>>> d1f94ccd1ff26aeb6ba94bd4a0c5755feb157a58
-
-router.post('/signup', authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
 router.get('/reset', authController.getReset);
 
-<<<<<<< HEAD
 router.post('/reset', authController.postReset);
 
 router.get('/reset/:token', authController.getNewPassword);
@@ -81,6 +75,3 @@ router.get('/reset/:token', authController.getNewPassword);
 router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> d1f94ccd1ff26aeb6ba94bd4a0c5755feb157a58
